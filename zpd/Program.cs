62e64e6995e163ff337f74bb653937be31e8b051
Jim Wordelman <quad341@gmail.com>
2012-05-12 16:19:44 -0700

    This is working locally finally though I gave up trying to get references correct and just drop into the Zune folder.
    Looking at things, the zune launch takes a bit of time, but seems to work afterwards. Now trying to add WCF to make this a simple service. There is going to be a ton of refactoring after this

--- zpd/Program.cs
+++ zpd/Program.cs
@@ -2,13 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using VosSoft.ZuneLcd.Api;
+using System.Threading;
+using System.ServiceModel;
+
+
 
 namespace zpd
 {
+    [ServiceContract(Namespace = "http://zpd")]
+    public interface IZPDClient
+    {
+        [OperationContract]
+        void Play();
+        [OperationContract]
+        void Pause();
+        [OperationContract]
+        void Close();
+    }
+
     class Program
     {
+        static ZuneApi zune;
         static void Main(string[] args)
         {
+            zune = new ZuneApi();
+            var zuneThread = new Thread(ZuneThread);
+            zuneThread.Start();
+            do
+            {
+                Thread.Sleep(1000);
+            }
+            while (!zune.IsReady);
+
+            zune.Play();
+            var c = Console.ReadKey();
+            zune.Close();
+            zuneThread.Join();
+        }
+
+        static void ZuneThread()
+        {
+            zune.Launch();
         }
     }
 }
