118185d1912337ffe8ddb3865b3fb65ab12330e4
Jim Wordelman <quad341@gmail.com>
2012-05-12 17:01:34 -0700

    This adds a basic testing client that will just call play and then close. It's all very basic/proof of concept but it seems to actually be working.

---
+++ TestingClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace TestingClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new ZPDServiceClient();
+            client.Play();
+            Console.WriteLine("Should be playing; waits 10 seconds to ensure player is open though");
+            Console.WriteLine("Press <enter> to close player and client");
+            Console.ReadLine();
+            client.Close();
+        }
+    }
+}
